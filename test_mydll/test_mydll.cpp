3aa81b4a8881e278499ec5e215d66e98cd824120
Nightstars <ChristZhangowner@gmail..com>
2020-06-04 17:58:11 +0800

    20200604

--- test_mydll/test_mydll.cpp
+++ test_mydll/test_mydll.cpp
@@ -8,7 +8,8 @@ using namespace std;
 #include<fstream>
 #include <vector>
 #include <numeric> // for std::accumulate
-
+#include<iostream>
+#include<fstream>
 
 int main()
 {
@@ -86,50 +87,81 @@ int main()
 
 #pragma region test_dll_dasource
 	//typedef int(*QBASEFUNC)(const char* db_url, const char* db_name, const char* db_sql, char** result);
-	typedef int(*QBASEFUNC)(const char* db_url, const char* db_name, const char* db_sql, char** result);
-	HINSTANCE hDllInst;
-	hDllInst = LoadLibrary("dtsource.dll");
-	QBASEFUNC q_rcnum = (QBASEFUNC)GetProcAddress(hDllInst, "q_rc");
-	const char* db_url =(char *)malloc(1024 * sizeof(char));
-	const char* db_name =(char *)malloc(128 * sizeof(char));
-	const char* dt_name =(char *)malloc(128 * sizeof(char));
-	const char* db_sql =(char *)malloc(4096 * sizeof(char));
-	db_url = "Provider=SQLOLEDB; Server=192.168.0.187,1433\MSSQLSERVER;Database=CMS; uid=sa; pwd=Ihavenoidea@0;";
-	db_name= "PUB_PARA";
-	dt_name= "CURR";
-	db_sql= "SELECT * FROM CURR";
-	int n(8);
-	char** pchar = NULL;
-	pchar = (char**)malloc(n* sizeof(char*)); // pchar其实就是一个char * []数组
-	for (int i(0); i < n; i++)
-	{
-		*(pchar + i) = (char*)malloc(64*1819*sizeof(char));
-	}
+	//HINSTANCE hDllInst;
+	//hDllInst = LoadLibrary("dtsource.dll");
+	//QBASEFUNC q_rcnum = (QBASEFUNC)GetProcAddress(hDllInst, "q_rc");
+	//const char* db_url =(char *)malloc(1024 * sizeof(char));
+	//const char* db_name =(char *)malloc(128 * sizeof(char));
+	//const char* dt_name =(char *)malloc(128 * sizeof(char));
+	//const char* db_sql =(char *)malloc(4096 * sizeof(char));
+	//db_url = "Provider=SQLOLEDB; Server=192.168.0.187,1433\MSSQLSERVER;Database=CMS; uid=sa; pwd=Ihavenoidea@0;";
+	//db_name= "PUB_PARA";
+	//dt_name= "CURR";
+	//db_sql= "SELECT * FROM CURR";
+	//int n(8);
+	//char** pchar = NULL;
+	//pchar = (char**)malloc(n* sizeof(char*)); // pchar其实就是一个char * []数组
+	//for (int i(0); i < n; i++)
+	//{
+	//	*(pchar + i) = (char*)malloc(64*1819*sizeof(char));
+	//}
 
-	int stat=q_rcnum(db_url,db_name,db_sql,pchar);
-	if (stat == 1) {
-		cout << "status：" << *(pchar + 0) << endl;
-		cout << "clumsize：" << *(pchar + 3) << endl;
-		cout << "clums：" << *(pchar + 4) << endl;
-		cout << "recoders：" << *(pchar + 5) << endl;
-		cout << "clum_name：" << *(pchar + 6) << endl;
-		cout << "rc_name：" << *(pchar + 7) << endl;
-	}
-	else {
-		cout << "status：" << *(pchar + 0) << endl;
-		cout << "Description：" <<*(pchar + 1) << endl;
-	}
-	
-	
-	//q_base(db_url, db_name, db_sql);
-	FreeLibrary(hDllInst);
-	for (int i(0); i < n; i++)
-	{
-		pchar[i] = nullptr;
-		free(pchar[i]);
-	}
-	free(pchar);
+	//int stat=q_rcnum(db_url,db_name,db_sql,pchar);
+	//if (stat == 1) {
+	//	cout << "status：" << *(pchar + 0) << endl;
+	//	cout << "clumsize：" << *(pchar + 3) << endl;
+	//	cout << "clums：" << *(pchar + 4) << endl;
+	//	cout << "recoders：" << *(pchar + 5) << endl;
+	//	cout << "clum_name：" << *(pchar + 6) << endl;
+	//	cout << "rc_name：" << *(pchar + 7) << endl;
+	//}
+	//else {
+	//	cout << "status：" << *(pchar + 0) << endl;
+	//	cout << "Description：" <<*(pchar + 1) << endl;
+	//}
+	//
+	//
+	//FreeLibrary(hDllInst);
+	//for (int i(0); i < n; i++)
+	//{
+	//	pchar[i] = nullptr;
+	//	free(pchar[i]);
+	//}
+	//free(pchar);
 #pragma endregion
+
+	//fstream fs("F:\\vsstudio\\mydll\\x64\\Debug\\test.txt", ios::binary | ios::out | ios::in);
+	////跳转到开头的第二个字节位置进行写入，正常写入
+	//fs.seekp(29, ios::beg);
+	//fs.write("\r\n!\r\n!\r\n!", 15);
+	//fs.close();
+
+	int i = 0, num;
+	char str[100][500] = { 0 }, linedata[100] = { 0 };
+	FILE* fp = fopen("F:\\vsstudio\\mydll\\x64\\Debug\\test.txt", "r");
+	FILE* fpw;
+	while (fgets(linedata, sizeof(linedata) - 1, fp))
+	{
+		if (strcmp(linedata, "[{$1$}]\n") == 0|| strcmp(linedata, "[{$1$}]") == 0)
+		{
+			strcpy(str[i], "this\nis\ngenc\ntest\n");
+		}
+		else
+		{
+			strcpy(str[i], linedata);
+		}
+		i++;
+	}
+	fclose(fp);
+	num = i;
+	fpw = fopen("F:\\vsstudio\\mydll\\x64\\Debug\\temp.txt", "w");
+	for (i = 0; i < num; i++)
+	{
+		fputs(str[i], fpw);
+	}
+	fclose(fpw);
+
+
 
 	system("pause");
 }
